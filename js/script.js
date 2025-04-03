2c32920d6ee560bbc67c1b2630a678aeed2e8567
Yevheniia Tsapar <ts.yevheniia@gmail.com>
2025-04-03 17:58:50 +0200

    Added First Section and Registration Form

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const navbar = document.getElementsByClassName('navbar')[0];
+const header = document.getElementsByClassName('header')[0];
+document.addEventListener('scroll',(e)=>{
+    const headerHeight = header.offsetHeight;
+    if(window.scrollY >=  headerHeight - 150){
+        console.log('scroll')
+        navbar.classList.add('bg-dark');
+    }
+    else {
+        navbar.classList.remove('bg-dark');
+    }
+})
