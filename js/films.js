421d0855bd9e394d72e3803135afddaf297c825d
Yevheniia Tsapar <ts.yevheniia@gmail.com>
2025-04-08 11:03:06 +0200

    Created 2 New Sections, such as: Upcoming and Popular

--- js/films.js
+++ js/films.js
@@ -1,8 +1,23 @@
-const activatedFilmsContainer = document.getElementById('activatedFilms');
-const filmsContainer = document.getElementById('films');
-const filmsContainer3 = document.getElementById('3films');
-const filmsContainer4 = document.getElementById('4films');
-const filmsContainer5 = document.getElementById('5films');
+const activatedTopRatedFilmsContainer = document.getElementById('activatedTopRatedFilms');
+const filmsTopRatedContainer = document.getElementById('topRatedFilms');
+const filmsTopRatedContainer3 = document.getElementById('topRatedFilms3');
+const filmsTopRatedContainer4 = document.getElementById('topRatedFilms4');
+const filmsTopRatedContainer5 = document.getElementById('topRatedFilms5');
+const activatedNowPlayingContainer = document.getElementById('activatedNowPlayingFilms');
+const nowPlayingContainer = document.getElementById('nowPlayingFilms');
+const nowPlayingContainer3 = document.getElementById('nowPlayingFilms3');
+const nowPlayingContainer4 = document.getElementById('nowPlayingFilms4');
+const nowPlayingContainer5 = document.getElementById('nowPlayingFilms5');
+const activatedUpcomingFilmsContainer = document.getElementById('activatedUpcomingFilms');
+const upcomingFilmsContainer = document.getElementById('upcomingFilms');
+const upcomingFilmsContainer3 = document.getElementById('upcomingFilms3');
+const upcomingFilmsContainer4 = document.getElementById('upcomingFilms4');
+const upcomingFilmsContainer5 = document.getElementById('upcomingFilms5');
+const activatedPopularFilmsContainer = document.getElementById('activatedPopularFilms');
+const popularFilmsContainer = document.getElementById('popularFilms');
+const popularFilmsContainer3 = document.getElementById('popularFilms3');
+const popularFilmsContainer4 = document.getElementById('popularFilms4');
+const popularFilmsContainer5 = document.getElementById('popularFilms5');
 const url = 'https://image.tmdb.org/t/p/w500/';
 
 
@@ -22,7 +37,20 @@ const optionsNowPlaying = {
     }
 };
 
-
+const optionsUpcoming = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYzc2MDljYzExMGM5NjgwZDhhMGNjMGVjNTJkMDI5YiIsIm5iZiI6MTc0NDAzODc2NC44MDQsInN1YiI6IjY3ZjNlYjZjNmMzNTgzYzk3NTk5MzVjNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8dsabqerc-bpXrjFYtlPGbNt3VQb27YltuN5f5REcks'
+    }
+};
+const optionsPopular = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYzc2MDljYzExMGM5NjgwZDhhMGNjMGVjNTJkMDI5YiIsIm5iZiI6MTc0NDAzODc2NC44MDQsInN1YiI6IjY3ZjNlYjZjNmMzNTgzYzk3NTk5MzVjNSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.8dsabqerc-bpXrjFYtlPGbNt3VQb27YltuN5f5REcks'
+    }
+};
 const getTopRatedFilms = function () {
     return fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', optionsTopRated)
         .then(res => res.json())
@@ -35,7 +63,18 @@ const getNowPlayingFilms = function () {
         .then(res => res.results)
         .catch(err => console.error(err));
 }
-
+const getUpcomingFilms = function () {
+    return fetch('https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1', optionsUpcoming)
+        .then(res => res.json())
+        .then(res => res.results)
+        .catch(err => console.error(err));
+}
+const getPopularFilms = function () {
+    return fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=2', optionsPopular)
+        .then(res => res.json())
+        .then(res => res.results)
+        .catch(err => console.error(err));
+}
 
 const renderTopRatedFilms = async function () {
     const data = await getTopRatedFilms();
@@ -43,61 +82,61 @@ const renderTopRatedFilms = async function () {
         html = `
          <div class="card mx-4" style="width: 16rem;">
            <img src="${url}${film.poster_path}" class="card-img-top" alt="...">
-           <a href="#" class="btn btn-primary">Go somewhere</a>
+           <a href="#" class="btn btn-primary mt-2">Watch Now</a>
            <div class="card-body">
              <h5 class="card-title">${film.title}</h5>
              <p class="card-text">${film.overview}</p>
            </div>
          </div>`;
-        activatedFilmsContainer.insertAdjacentHTML('beforeend', html)
+        activatedTopRatedFilmsContainer.insertAdjacentHTML('beforeend', html)
     })
     data.slice(4,8).forEach(film => {
         html = `
          <div class="card mx-4" style="width: 16rem;">
            <img src="${url}${film.poster_path}" class="card-img-top" alt="...">
+           <a href="#" class="btn btn-primary mt-2">Watch Now</a>
            <div class="card-body">
              <h5 class="card-title">${film.title}</h5>
              <p class="card-text">${film.overview}</p>
-             <a href="#" class="btn btn-primary">Go somewhere</a>
            </div>
          </div>`;
-        filmsContainer.insertAdjacentHTML('beforeend', html)
+        filmsTopRatedContainer.insertAdjacentHTML('beforeend', html)
     })
     data.slice(8,12).forEach(film => {
         html = `
          <div class="card mx-4" style="width: 16rem;">
            <img src="${url}${film.poster_path}" class="card-img-top" alt="...">
+           <a href="#" class="btn btn-primary mt-2">Watch Now</a>
            <div class="card-body">
              <h5 class="card-title">${film.title}</h5>
              <p class="card-text">${film.overview}</p>
-             <a href="#" class="btn btn-primary">Go somewhere</a>
            </div>
          </div>`;
-        filmsContainer3.insertAdjacentHTML('beforeend', html)
+        filmsTopRatedContainer3.insertAdjacentHTML('beforeend', html)
     })
     data.slice(12,16).forEach(film => {
         html = `
          <div class="card mx-4" style="width: 16rem;">
            <img src="${url}${film.poster_path}" class="card-img-top" alt="...">
+           <a href="#" class="btn btn-primary mt-2">Watch Now</a>
            <div class="card-body">
              <h5 class="card-title">${film.title}</h5>
              <p class="card-text">${film.overview}</p>
-             <a href="#" class="btn btn-primary">Go somewhere</a>
            </div>
          </div>`;
-        filmsContainer4.insertAdjacentHTML('beforeend', html)
+        filmsTopRatedContainer4.insertAdjacentHTML('beforeend', html)
     })
     data.slice(16,20).forEach(film => {
         html = `
          <div class="card mx-4" style="width: 16rem;">
            <img src="${url}${film.poster_path}" class="card-img-top" alt="...">
+           <a href="#" class="btn btn-primary mt-2">Watch Now</a>
            <div class="card-body">
              <h5 class="card-title">${film.title}</h5>
              <p class="card-text">${film.overview}</p>
-             <a href="#" class="btn btn-primary">Go somewhere</a>
            </div>
          </div>`;
-        filmsContainer5.insertAdjacentHTML('beforeend', html)
+        filmsTopRatedContainer5.insertAdjacentHTML('beforeend', html)
     })
 }
 renderTopRatedFilms()
@@ -107,61 +146,189 @@ const renderNowPlayingFilms = async function () {
         html = `
          <div class="card mx-4" style="width: 16rem;">
            <img src="${url}${film.poster_path}" class="card-img-top" alt="...">
-           <a href="#" class="btn btn-primary">Go somewhere</a>
+           <a href="#" class="btn btn-primary mt-2">Watch Now</a>
            <div class="card-body">
              <h5 class="card-title">${film.title}</h5>
              <p class="card-text">${film.overview}</p>
            </div>
          </div>`;
-        activatedFilmsContainer.insertAdjacentHTML('beforeend', html)
+        activatedNowPlayingContainer.insertAdjacentHTML('beforeend', html)
     })
     data.slice(4,8).forEach(film => {
         html = `
          <div class="card mx-4" style="width: 16rem;">
            <img src="${url}${film.poster_path}" class="card-img-top" alt="...">
+            <a href="#" class="btn btn-primary mt-2">Watch Now</a>
            <div class="card-body">
              <h5 class="card-title">${film.title}</h5>
              <p class="card-text">${film.overview}</p>
-             <a href="#" class="btn btn-primary">Go somewhere</a>
            </div>
          </div>`;
-        filmsContainer.insertAdjacentHTML('beforeend', html)
+        nowPlayingContainer.insertAdjacentHTML('beforeend', html)
     })
     data.slice(8,12).forEach(film => {
         html = `
          <div class="card mx-4" style="width: 16rem;">
            <img src="${url}${film.poster_path}" class="card-img-top" alt="...">
+           <a href="#" class="btn btn-primary mt-2">Watch Now</a>
            <div class="card-body">
              <h5 class="card-title">${film.title}</h5>
              <p class="card-text">${film.overview}</p>
-             <a href="#" class="btn btn-primary">Go somewhere</a>
            </div>
          </div>`;
-        filmsContainer3.insertAdjacentHTML('beforeend', html)
+        nowPlayingContainer3.insertAdjacentHTML('beforeend', html)
     })
     data.slice(12,16).forEach(film => {
         html = `
          <div class="card mx-4" style="width: 16rem;">
            <img src="${url}${film.poster_path}" class="card-img-top" alt="...">
+            <a href="#" class="btn btn-primary mt-2">Watch Now</a>
            <div class="card-body">
              <h5 class="card-title">${film.title}</h5>
              <p class="card-text">${film.overview}</p>
-             <a href="#" class="btn btn-primary">Go somewhere</a>
            </div>
          </div>`;
-        filmsContainer4.insertAdjacentHTML('beforeend', html)
+        nowPlayingContainer4.insertAdjacentHTML('beforeend', html)
     })
     data.slice(16,20).forEach(film => {
         html = `
          <div class="card mx-4" style="width: 16rem;">
            <img src="${url}${film.poster_path}" class="card-img-top" alt="...">
+           <a href="#" class="btn btn-primary mt-2">Watch Now</a>
            <div class="card-body">
              <h5 class="card-title">${film.title}</h5>
              <p class="card-text">${film.overview}</p>
-             <a href="#" class="btn btn-primary">Go somewhere</a>
            </div>
          </div>`;
-        filmsContainer5.insertAdjacentHTML('beforeend', html)
+        nowPlayingContainer5.insertAdjacentHTML('beforeend', html)
     })
 }
 renderNowPlayingFilms()
+const renderUpcomingFilms = async function () {
+    const data = await getUpcomingFilms();
+    data.slice(0,4).forEach(film => {
+        html = `
+         <div class="card mx-4" style="width: 16rem;">
+           <img src="${url}${film.poster_path}" class="card-img-top" alt="...">
+           <a href="#" class="btn btn-primary mt-2">Watch Now</a>
+           <div class="card-body">
+             <h5 class="card-title">${film.title}</h5>
+             <p class="card-text">${film.overview}</p>
+           </div>
+         </div>`;
+        activatedUpcomingFilmsContainer.insertAdjacentHTML('beforeend', html)
+    })
+    data.slice(4,8).forEach(film => {
+        html = `
+         <div class="card mx-4" style="width: 16rem;">
+           <img src="${url}${film.poster_path}" class="card-img-top" alt="...">
+            <a href="#" class="btn btn-primary mt-2">Watch Now</a>
+           <div class="card-body">
+             <h5 class="card-title">${film.title}</h5>
+             <p class="card-text">${film.overview}</p>
+           </div>
+         </div>`;
+        upcomingFilmsContainer.insertAdjacentHTML('beforeend', html)
+    })
+    data.slice(8,12).forEach(film => {
+        html = `
+         <div class="card mx-4" style="width: 16rem;">
+           <img src="${url}${film.poster_path}" class="card-img-top" alt="...">
+           <a href="#" class="btn btn-primary mt-2">Watch Now</a>
+           <div class="card-body">
+             <h5 class="card-title">${film.title}</h5>
+             <p class="card-text">${film.overview}</p>
+           </div>
+         </div>`;
+        upcomingFilmsContainer3.insertAdjacentHTML('beforeend', html)
+    })
+    data.slice(12,16).forEach(film => {
+        html = `
+         <div class="card mx-4" style="width: 16rem;">
+           <img src="${url}${film.poster_path}" class="card-img-top" alt="...">
+            <a href="#" class="btn btn-primary mt-2">Watch Now</a>
+           <div class="card-body">
+             <h5 class="card-title">${film.title}</h5>
+             <p class="card-text">${film.overview}</p>
+           </div>
+         </div>`;
+        upcomingFilmsContainer4.insertAdjacentHTML('beforeend', html)
+    })
+    data.slice(16,20).forEach(film => {
+        html = `
+         <div class="card mx-4" style="width: 16rem;">
+           <img src="${url}${film.poster_path}" class="card-img-top" alt="...">
+           <a href="#" class="btn btn-primary mt-2">Watch Now</a>
+           <div class="card-body">
+             <h5 class="card-title">${film.title}</h5>
+             <p class="card-text">${film.overview}</p>
+           </div>
+         </div>`;
+        upcomingFilmsContainer5.insertAdjacentHTML('beforeend', html)
+    })
+}
+renderUpcomingFilms()
+const renderPopularFilms = async function () {
+    const data = await getPopularFilms();
+    data.slice(0,4).forEach(film => {
+        html = `
+         <div class="card mx-4" style="width: 16rem;">
+           <img src="${url}${film.poster_path}" class="card-img-top" alt="...">
+           <a href="#" class="btn btn-primary mt-2">Watch Now</a>
+           <div class="card-body">
+             <h5 class="card-title">${film.title}</h5>
+             <p class="card-text">${film.overview}</p>
+           </div>
+         </div>`;
+        activatedPopularFilmsContainer.insertAdjacentHTML('beforeend', html)
+    })
+    data.slice(4,8).forEach(film => {
+        html = `
+         <div class="card mx-4" style="width: 16rem;">
+           <img src="${url}${film.poster_path}" class="card-img-top" alt="...">
+            <a href="#" class="btn btn-primary mt-2">Watch Now</a>
+           <div class="card-body">
+             <h5 class="card-title">${film.title}</h5>
+             <p class="card-text">${film.overview}</p>
+           </div>
+         </div>`;
+        popularFilmsContainer.insertAdjacentHTML('beforeend', html)
+    })
+    data.slice(8,12).forEach(film => {
+        html = `
+         <div class="card mx-4" style="width: 16rem;">
+           <img src="${url}${film.poster_path}" class="card-img-top" alt="...">
+           <a href="#" class="btn btn-primary mt-2">Watch Now</a>
+           <div class="card-body">
+             <h5 class="card-title">${film.title}</h5>
+             <p class="card-text">${film.overview}</p>
+           </div>
+         </div>`;
+        popularFilmsContainer3.insertAdjacentHTML('beforeend', html)
+    })
+    data.slice(12,16).forEach(film => {
+        html = `
+         <div class="card mx-4" style="width: 16rem;">
+           <img src="${url}${film.poster_path}" class="card-img-top" alt="...">
+            <a href="#" class="btn btn-primary mt-2">Watch Now</a>
+           <div class="card-body">
+             <h5 class="card-title">${film.title}</h5>
+             <p class="card-text">${film.overview}</p>
+           </div>
+         </div>`;
+        popularFilmsContainer4.insertAdjacentHTML('beforeend', html)
+    })
+    data.slice(16,20).forEach(film => {
+        html = `
+         <div class="card mx-4" style="width: 16rem;">
+           <img src="${url}${film.poster_path}" class="card-img-top" alt="...">
+           <a href="#" class="btn btn-primary mt-2">Watch Now</a>
+           <div class="card-body">
+             <h5 class="card-title">${film.title}</h5>
+             <p class="card-text">${film.overview}</p>
+           </div>
+         </div>`;
+        popularFilmsContainer5.insertAdjacentHTML('beforeend', html)
+    })
+}
+renderPopularFilms()
